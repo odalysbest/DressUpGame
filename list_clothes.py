6af0ea4abf17ad72f6d6a0110507336bb2546bcf
Katie Thai-Tang <kthaitang@gmail.com>
2019-11-15 14:54:40 -0500

    Add folder for icons

---
+++ list_clothes.py
@@ -0,0 +1,7 @@
+list_icons = ['hat-icon.png', 'shirt-icon.png', 'pant-icon.png', 'dress-icon.png', 'shoe-icon.png']
+
+list_hats = []
+list_shirts = []
+list_pants = []
+list_dresses = []
+list_shoes = []
