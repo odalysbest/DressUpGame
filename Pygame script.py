dce790846e0d06d13ef9620582750bfbe70671de
odalysbest <obenitez@olin.edu>
2019-11-19 14:24:26 -0500

    sample screen clicking

--- Pygame script.py
+++ Pygame script.py
@@ -52,6 +52,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if
             pass
         if event.type == pygame.MOUSEBUTTONUP:
             pass
@@ -60,3 +61,37 @@ while running:
     pygame.time.delay(int(1000/fps))
 
 pygame.quit()
+#FOLLOWING CODE IS FOR SCREEN BUTTONS TO CHANGE: background, reset clothes,
+mouse = pygame.mouse.get_pos()
+click = pygame.mouse.get_pressed()
+   if menu == 'menu':
+       # Displays main options menu
+       # Looks for key presses to navigate through options (works great)
+       # Looks for mouse position and presses:
+       if 90 < mouse[0] < 110 and 90 < mouse[1] < 110:
+           if click[0] == 1:
+               menu = 'bgd'
+       elif 90 < mouse[0] < 110 and 190 < mouse[1] < 210:
+           if click[0] == 1:
+               menu = 'shirt'
+       elif 90 < mouse[0] < 110 and 290 < mouse[1] < 310:
+           if click[0] == 1:
+               menu = 'shoe'
+       elif 90 < mouse[0] < 110 and 390 < mouse[1] < 410:
+           if click[0] == 1:
+               menu = 'pant'
+       elif 90 < mouse[0] < 110 and 490 < mouse[1] < 510:
+           if click[0] == 1:
+               menu = 'dress'
+
+   if menu == 'attack':
+       # Displays the different attacks menu
+       # Since the attack screen shares some button coordinates,
+       # after switching menu = 'attack' it starts scanning for new input
+       # relating to the new coordinates. And since the coordinates are
+       # the same, and it still detects the button being pressed, it activates
+       # the next button.
+       # If you want to go back to the previous menu you'd just switch menu = 'menu' again
+   if menu == 'item':
+       # Displays the Item menu
+       # ...
