cd6e8c7dc0e218cf7a0edaf7c1fdb10955696b1e
odalysbest <obenitez@olin.edu>
2019-12-06 16:13:31 -0500

    adding clothes

--- PyMain.py
+++ PyMain.py
@@ -53,10 +53,3 @@ class PyManMain:
         game = True
         self.screen.fill((0,0,0))
         background = newScreen(files[index], [0,0])
-
-class newScreen(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-        self.image = pygame.image.load(image_file)
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
